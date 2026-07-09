e62f6365c93cd8f91c4929600f80a78108baf816
maulana firdaus <maulanaauliyafirdaus11@gmail.com>
2026-07-10 06:02:32 +0700

    tugas 11

--- 11/tugas/CSVReader.java
+++ 11/tugas/CSVReader.java
@@ -5,19 +5,20 @@ import java.io.IOException;
 
 public class CSVReader {
     public static void main(String[] args) {
-        String CSVFile = "D:\\FIRDAUS\\Tugas Semester 4\\Pemrograman Berorientasi Objek\\11\\tugas\\students.csv";
+        String csvFile = "D:\\FIRDAUS\\Tugas Semester 4\\Pemrograman Berorientasi Objek\\11\\tugas\\students.csv";
         String line;
         String csvSplitBy = ",";
         int indeks = 0;
         System.out.println("NIM, NAMA, UMUR, PRODI");
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))){
-            while (line = br.readLine() != null) {
+            while ((line = br.readLine()) != null) {
                 indeks++;
                 if (indeks>1) {
                     String[] student = line.split(csvSplitBy);
-                System.out.println(student[0] + "," + student[1] + "," +);                }
+                System.out.println(student[0] + "," + student[1] + ",");                }
             }
         } catch (Exception e) {
+            e.printStackTrace();
             // TODO: handle exception
         }
     }    
